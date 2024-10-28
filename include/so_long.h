01097b9a2c3882182f83b02273a60e85aca4b3ae
Antonio Luis Moreno Del Pino <antonimo@c3r2s3.42malaga.com>
2024-10-28 14:25:29 +0100

    modular functions

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: antonimo <antonimo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 12:35:07 by antonimo          #+#    #+#             */
-/*   Updated: 2024/10/18 11:52:06 by antonimo         ###   ########.fr       */
+/*   Updated: 2024/10/28 13:24:25 by antonimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,12 @@
 # include "../lib/minilibx-linux/mlx.h"
 # include "../lib/minilibx-linux/mlx_int.h"
 # include <stdbool.h>
+
+typedef struct s_entities
+{
+	int	player_counter;
+	int	exit_counter;
+	int	collectible_counter;
+}	t_entities;
 
 #endif
--- src/so_long_utils.c
+++ src/so_long_utils.c
@@ -6,7 +6,7 @@
 /*   By: antonimo <antonimo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 10:24:07 by antonimo          #+#    #+#             */
-/*   Updated: 2024/10/18 13:52:11 by antonimo         ###   ########.fr       */
+/*   Updated: 2024/10/28 14:25:07 by antonimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,44 +16,76 @@
 {
 	if (ac == X)
 	{
-
+		map_validation(av);
 	}
 } */
 
-void	map_validation(char	*map)
+void	map_validation(char *map)
 {
 	int			fd;
-	int			len;
 	char		*line;
+	int			len;
+	t_entities	entities;
 
 	fd = open(map, O_RDONLY);
+	if (fd == -1)
+		exit(EXIT_FAILURE);
 	line = get_next_line(fd);
 	if (!line)
-		//salirse porque el mapa está NULL
-	if (!map_edges(line, '1'))
-		//salirse porque el mapa no tiene bien los edges
+		exit(EXIT_FAILURE);
+	validate_map_edges(&line);
 	len = ft_strlen(line);
 	while (line)
 	{
-		if (!check_len(line, len))
-			//salir
-		if (!map_body(line, '1'))
-			//salir
+		validate_line(&line, len);
+		count_entities(line, &entities);
 		line = get_next_line(fd);
 	}
-	if (!map_edges(line, '1'))
-		//salirse porque el mapa no tiene bien los edges
-	printf("%d", len);
+	check_entities(entities, &line);
 }
 
-// Funcion que comprueba si el string está lleno del caracter c
-bool	map_edges(char *str, char c)
+bool	validate_line(char **line, int len)
 {
-	if (!all_chars_same(str, c))
-		return (false);
-	return (true);
+	validate_body(line, len);
+	validate_map_edges(line);
 }
 
+void	check_entities(t_entities entities, char **line)
+{
+	if (entities.player_counter != 1 || entities.exit_counter != 1
+		|| entities.collectible_counter < 1)
+	{
+		free(line);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	count_entities(char *line, t_entities *entities)
+{
+	entities->player_counter += check_strchr(line, 'P');
+	entities->exit_counter += check_strchr(line, 'E');
+	entities->collectible_counter += check_strchr(line, 'C');
+}
+
+void	validate_body(char *line, int len)
+{
+	if (!check_len(line, len) || !map_body(line, '1'))
+	{
+		free(line);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	validate_map_edges(char **line)
+{
+	if (!&line || !all_chars_same(line, '1'))
+	{
+		free(&line);
+		exit(EXIT_FAILURE);
+	}
+}
+
+// Función que comprueba que todos los chr sean iguales
 bool	all_chars_same(char *str, char c)
 {
 	if (!str)
@@ -67,19 +99,18 @@ bool	all_chars_same(char *str, char c)
 	return (true);
 }
 
+// Función que comprueba que la primera letra y la ultima sean el chr 'c'
 bool	map_body(char *str, char c)
 {
 	int	len;
 
 	len = (ft_strlen(str));
-	if (str[0] != c)
+	if (str[0] == c)
 	{
-		if (len > 0 && str[len] == '\n')
-			len--;
-		if (str[len] != c)
-			return (false);
+		if (str[--len] == c)
+			return (true);
 	}
-	return (true);
+	return (false);
 }
 
 // Si str tiene un len diferente al que le paso, false.
@@ -90,11 +121,7 @@ bool	check_len(char *str, int len)
 	return (true);
 }
 
-bool	count_collectibles(char *str, char c)
-{
-	return (count_char_in_str);
-}
-
+// Función que cuenta cuantas coincidencias de 'c' hay en un str
 int	count_char_in_str(char *str, char c)
 {
 	int	res;
@@ -112,6 +139,7 @@ int	count_char_in_str(char *str, char c)
 	return (res);
 }
 
+// Función que devuelve True en caso de que solo exista 1 coincidencia de 'c' en str
 bool	one_char_in_str(str, c)
 {
 	int	count;
@@ -120,13 +148,4 @@ bool	one_char_in_str(str, c)
 	if (count != 1)
 		return (false);
 	return (true);
-}
-bool	player_exist(char *str, char c)
-{
-	return (one_char_in_str(str, c));
-}
-
-bool	exit_exist(char *str, char c)
-{
-	return (one_char_in_str(str, c));
 }
